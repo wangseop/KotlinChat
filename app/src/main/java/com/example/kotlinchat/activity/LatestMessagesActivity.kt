53c533141feb5189d0d7e670dcd8496c645b2706
wangseop <pondering112@gmail.com>
2019-12-24 03:34:19 +0900

    chatbot chat list 목록 xml 파일 작성 중

---
+++ app/src/main/java/com/example/kotlinchat/activity/LatestMessagesActivity.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinchat.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kotlinchat.R
+
+class LatestMessagesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_latest_messages)
+    }
+}
